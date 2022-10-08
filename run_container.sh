68e74a71b28d98ee18220f3017259970b318efad
EduardoY228 <eduardyermolenkowork@gmail.com>
2022-10-08 20:46:04 +0200

    ci/cd

--- run_container.sh
+++ run_container.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-container=$(docker ps | grep 'ses-monitoring-js' | awk '{ print $1 }')
+container=$(docker ps | grep 'helios-js' | awk '{ print $1 }')
 
 if [ -z "$container" ]
   then
       echo "ses-monitoring-js container not running"
       echo "starting container..."
-      docker run -d -p3034:3034 ses-monitoring-js
+      docker run -d --name helios-js -p3034:3034 ses-monitoring-js
   else
-     echo "ses-monitoring-js container running, id: $container"
-     echo "rerun docker container"
-     docker stop $container
-     echo "starting container..."
-     docker run -d -p3034:3034 ses-monitoring-js
+      echo "ses-monitoring-js container running, id: $container"
+      echo "rerun docker container"
+      docker stop $container
+      echo "starting container..."
+      docker run -d --name helios-js -p3034:3034 ses-monitoring-js
 fi
